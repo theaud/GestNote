d89d2d3caffb570bb390bc8f7886df96058a8a62
theaud <brionmathieu@gmail.com>
2016-02-04 09:38:08 +0100

    inserdata base commit 1

--- insertData.php
+++ insertData.php
@@ -6,12 +6,49 @@
  * and open the template in the editor.
  */
 
-function inser_data_vehicle($data_base,$Make,$Model,$Year,$CC,$Colour,$Picture)
+
+//--------------------------------------------------------------------------------------------------------
+////-----------------------------------   Student       --------------------------------------------------
+//--------------------------------------------------------------------------------------------------------
+function inser_data_student($data_base,$id,$surname,$forename,$contact,$doctor,$marks)
 {
-$SQL = "INSERT INTO `b026198f`.`car` (`Make`, `Model`, `Year`, `CC`, `Colour`, `Picture`) 
-VALUES ('".$Make."', '".$Model."', '".$Year."', '".$CC."', '".$Colour."', '".$Picture."')";
+$SQL = "INSERT INTO `GestNotes`.`student` (`id`, `surname`, `forename`, `contact`, `doctor`, `marks`) 
+VALUES ('".$id."', '".$surname."', '".$forename."', '".$contact."', '".$doctor."', '".$marks."')";
 $result = $data_base->query ($SQL);
 
 if (!$result) { die('Requête invalide : ' . mysql_error());}
 }
 
+function inser_data_student_raw($data_base,$id,$contact,$doctor)
+{
+$SQL = "INSERT INTO `GestNotes`.`student` (`id`, `surname`, `forename`, `contact`, `doctor`, `marks`) 
+VALUES ('".$id."', 'Surname', 'Forename.', '".$contact."', '".$doctor."', 'Marks')";
+$result = $data_base->query ($SQL);
+
+if (!$result) { die('Requête invalide : ' . mysql_error());}
+}
+
+//--------------------------------------------------------------------------------------------------------
+////-----------------------------------   identity       --------------------------------------------------
+//--------------------------------------------------------------------------------------------------------
+function inser_data_identity($data_base,$id,$birth,$sex,$registerdate,$prev_school,$marks)
+{
+$SQL = "INSERT INTO `GestNotes`.`student` (`id`, `birth`, `sex`, `registerdate`, `prev_school`, `photo`) 
+VALUES ('".$id."', '".$birth."', '".$sex."', '".$registerdate."', '".$prev_school."', '".$marks."')";
+$result = $data_base->query ($SQL);
+
+if (!$result) { die('Requête invalide : ' . mysql_error());}
+}
+
+
+//--------------------------------------------------------------------------------------------------------
+////-----------------------------------   coordonate       --------------------------------------------------
+//--------------------------------------------------------------------------------------------------------
+function inser_data_coordonate($data_base,$id,$address,$postalcode,$city,$fixphone,$mobilephone)
+{
+$SQL = "INSERT INTO `GestNotes`.`student` (`id`, `birth`, `sex`, `registerdate`, `prev_school`, `photo`) 
+VALUES ('".$id."', '".$address."', '".$postalcode."', '".$city."', '".$fixphone."', '".$mobilephone."')";
+$result = $data_base->query ($SQL);
+
+if (!$result) { die('Requête invalide : ' . mysql_error());}
+}
